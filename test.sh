fbe9922e4a6099983831ee8f54cf3b16b6d1b209
ikhwan.affan <ikhwanaffan00@gmail.com>
2026-09-23 21:51:45 -0400

    add read all users expiry date

--- test.sh
+++ test.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-USERNAME="affan"
-EXPIRY_DATE=$(sudo chage -l "$USERNAME" | grep "Account expires" | cut -d ':' -f 2 | xargs)
-EXPIRY_TIMESTAMP=$(date -d "$EXPIRY_DATE" +%s)
-CURRENT_TIMESTAMP=$(date +%s)
+USERNAME=$(getent passwd | awk -F: '$3 >= 1000 && $3 < 65534{print $1}')
 
-if [ "$CURRENT_TIMESTAMP" -ge "$EXPIRY_TIMESTAMP" ]; then
-	echo "Account $USERNAME has expired. Deleting user..."
-	echo "Would delete: $USERNAME"
-	userdel -r "$USERNAME"
-else
-echo "Account $USERNAME has not expired."
-fi
+for USER in $USERNAME; do
+
+	EXPIRY_DATE=$(sudo chage -l "$USERNAME" | grep "Account expires" | cut -d ':' -f 2 | xargs)
+
+	if [ "$EXPIRY_DATE" = "never"]; then
+		continue
+	fi 
+	
+	EXPIRY_TIMESTAMP=$(date -d "$EXPIRY_DATE" +%s)
+	CURRENT_TIMESTAMP=$(date +%s)
+
+	if [ "$CURRENT_TIMESTAMP" -ge "$EXPIRY_TIMESTAMP" ]; then
+		echo "Account $USERNAME has expired. Deleting user..."
+		echo "Would delete: $USERNAME"
+		#userdel -r "$USERNAME"
+	else
+		echo "Account $USERNAME has not expired."
+	fi
+done
